190c4b71f5ab8adefba0ad3f27cd2ffa44ba2a17
Max <maxvphillips@gmail.com>
2025-12-28 16:51:42 -0600

    Auto-sync: 2025-12-28

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,3 +1,21 @@
+function __styled_pwd
+    set -l parts (string split / (prompt_pwd))
+    set -l last (count $parts)
+
+    for i in (seq 1 $last)
+        if test $i -lt $last
+            set_color brblack
+        else
+            set_color blue
+        end
+        echo -n $parts[$i]
+        test $i -lt $last; and echo -n '/'
+    end
+
+    set_color normal
+end
+
+
 function fish_prompt
     set -l last_status $status
 
--- fish/.config/fish/functions/fish_right_prompt.fish
+++ fish/.config/fish/functions/fish_right_prompt.fish
@@ -4,11 +4,24 @@ function __git_info
     set -l branch (command git symbolic-ref --short HEAD 2>/dev/null \
         || command git rev-parse --short HEAD 2>/dev/null)
 
-    command git diff --quiet --ignore-submodules HEAD >/dev/null 2>&1
-    or set -l dirty '*'
+    set -l status (command git status --porcelain=2 --branch 2>/dev/null)
 
-    echo "$branch$dirty"
+    set -l staged   (string match -r '^1 ' $status | count)
+    set -l unstaged (string match -r '^2 ' $status | count)
+
+    set -l ahead  (string match -r 'ahead ([0-9]+)'  $status | string replace -r '.* ' '')
+    set -l behind (string match -r 'behind ([0-9]+)' $status | string replace -r '.* ' '')
+
+    set -l out "$branch"
+
+    test $staged   -gt 0; and set out "$out +$staged"
+    test $unstaged -gt 0; and set out "$out ~$unstaged"
+    test -n "$ahead";  and set out "$out ↑$ahead"
+    test -n "$behind"; and set out "$out ↓$behind"
+
+    echo $out
 end
+
 
 function fish_right_prompt
     set -g fish_prompt_pwd_dir_length 3
